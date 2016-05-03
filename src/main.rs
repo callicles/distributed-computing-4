0b746eeee204a662edb3d82987668f8e132ad2a5
Nicolas Joseph <nicolas@nicolasjoseph.com>
2016-05-03 13:32:07 -0400

    Fixed some of the border extraction and add some test for shadow borders

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 extern crate convolution;
 extern crate threadpool;
-use std::sync::mpsc::channel;
 use std::sync::Arc;
 use convolution::convolute::*;
 use convolution::utils::*;
@@ -14,8 +13,8 @@ use std::env;
 
 fn main() {
 
-    const x: usize = 4;
-    const y: usize = 4;
+    const X: usize = 4;
+    const Y: usize = 4;
 
     //  Getting image path and stencils
     let args = env::args();
@@ -23,7 +22,7 @@ fn main() {
     let mut num_cpus = 1;
     let mut image_path = "".to_string();
     let mut stencils = Vec::new();
-    
+
     for (i, j) in args.enumerate(){
         if i == 1 {
             let cpu_input = j.clone().parse::<usize>().unwrap();
@@ -42,14 +41,14 @@ fn main() {
     }
 
     let loaded_img = img_from_file(image_path);
-    
+
 
     let stencil: Vec<Vec<f32>> = stencil_from_file(stencils.first().unwrap());
 
     let shared_stencil =  Arc::new(stencil);
     let shared_img =  Arc::new(loaded_img);
 
-    static mut output_image: [[(f32, f32, f32);x];y] = [[(0.0,0.0,0.0); x]; y];
+    static mut output_image: [[(f32, f32, f32);X];Y] = [[(0.0,0.0,0.0); X]; Y];
 
 
     let mut handles = Vec::new();
@@ -58,12 +57,12 @@ fn main() {
         let stencil_ref = shared_stencil.clone();
         let img_ref = shared_img.clone();
         println!("{:?}", i);
-        
-        
+
+
         let handle = thread::spawn(move || {
             unsafe {
                 let result = compute_pixel(&stencil_ref, Coordinate(i as isize,i as isize), &img_ref.clone());
-                
+
                 output_image[i as usize][i as usize]= (result.0, result.1, result.2);
                 println!("{:?}", result);
             }
@@ -74,11 +73,11 @@ fn main() {
     for h in handles {
         h.join();
     }
-    
+
     unsafe{
-        println!("{:?}", output_image);    
+        println!("{:?}", output_image);
     }
-    
+
 
     // println!("Pixel value computed: {}", result[0].0);
 }
